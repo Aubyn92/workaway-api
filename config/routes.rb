3fb94d2de5c78ff8b79af88b5e39b4b226496813
cdask <d.ask3@yahoo.com>
2020-07-10 03:54:42 +1000

    Initial Authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :hosts
+  post "/login/", to: 'usertoken#create'
+  get "/status", to: "status#index"
+  get "/status/user", to: "status#user"
+  post "/sign-up", to: "users#create"
 end
